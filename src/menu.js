792b19d1bb1dfa38d50b61ae0aac5d8b56ffa296
Oleh Buyan <olegaddeus@gmail.com>
2025-05-31 21:19:50 +0400

    version-3

--- src/menu.js
+++ src/menu.js
@@ -1,36 +1,32 @@
-import {Menu} from './core/menu'
-import {BackgroundModule} from './modules/background.module'
-import {RandomMessageModule} from './modules/random.message.module'
-import {RandomSoundModule} from './modules/random.sound.module'
+import { Menu } from "./core/menu";
+import { BackgroundModule } from "./modules/background.module";
+import { RandomMessageModule } from "./modules/random.message.module";
+import { RandomSoundModule } from "./modules/random.sound.module";
 import { ShapeModule } from "./modules/shape.module";
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
 
-    constructor(selector) {
+    this.modules = [];
 
-      super(selector)
-
-      this.modules = []
-
-      document.body.addEventListener('contextmenu', (event) => {
-        event.preventDefault()
-        this.open(event.clientX, event.clientY) 
-      })
+    document.body.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      this.open(event.clientX, event.clientY);
+    });
   }
 
   open(y, x) {
-    const menu = document.querySelector('ul')
-    menu.classList.toggle('open')
-    
+    const menu = document.querySelector("ul");
+    menu.classList.toggle("open");
     menu.style.top = `${x}px`;
     menu.style.left = `${y}px`;
   }
 
   close() {
-
     this.el.classList.remove("open");
   }
-  
+
   findIndex(event) {
     console.log(event);
     if (event.target.offsetParent === this.el) {
@@ -44,24 +40,32 @@ export class ContextMenu extends Menu {
   add() {
     document.body.addEventListener("click", this.findIndex.bind(this));
 
-    console.log('Add background module')
-    const background_module = new BackgroundModule('Background', 'Случайный фон')
-    this.el.insertAdjacentHTML('beforeend', background_module.toHTML())
-    this.modules.push(background_module)
+    console.log("Add background module");
+    const background_module = new BackgroundModule(
+      "Background",
+      "Случайный фон"
+    );
+    this.el.insertAdjacentHTML("beforeend", background_module.toHTML());
+    this.modules.push(background_module);
 
-    console.log('Add random message module')
-    const random_message_module = new RandomMessageModule('RandomMessage', 'Рандомное сообщение')
-    this.el.insertAdjacentHTML('beforeend', random_message_module.toHTML())
-    this.modules.push(random_message_module)
+    console.log("Add random message module");
+    const random_message_module = new RandomMessageModule(
+      "RandomMessage",
+      "Рандомное сообщение"
+    );
+    this.el.insertAdjacentHTML("beforeend", random_message_module.toHTML());
+    this.modules.push(random_message_module);
 
-    console.log('Add random sound module')
-    const random_sound_module = new RandomSoundModule('RandomSound', 'Рандомный звук')
-    this.el.insertAdjacentHTML('beforeend', random_sound_module.toHTML())
-    this.modules.push(random_sound_module)
-    
-    const click = new ShapeModule('Shape', "Нарисуй фигуру");
-    this.el.innerHTML += click.toHTML();
-    this.modules.push(click);
-  };
+    console.log("Add random sound module");
+    const random_sound_module = new RandomSoundModule(
+      "RandomSound",
+      "Рандомный звук"
+    );
+    this.el.insertAdjacentHTML("beforeend", random_sound_module.toHTML());
+    this.modules.push(random_sound_module);
 
+    const shape = new ShapeModule("Shape", "Нарисуй фигуру");
+    this.el.insertAdjacentHTML("beforeend", shape.toHTML());
+    this.modules.push(shape);
+  }
 }
