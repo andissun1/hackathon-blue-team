01f06351d9e15e173c2d688f90ebd4b5dd925cc1
Андрей <Winterles@yandex.ru>
2025-05-31 11:13:22 +0300

    Вернул код от Олега

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,17 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
-  
+  constructor(type, text) {
+    super(type, text);
+  }
+
+  trigger() {
+    document.body.style.backgroundColor = `red`;
+
+    // throw new Error(`Trigger method should be implemented in module "${this.type}"`)
+  }
+
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`;
+  }
 }
